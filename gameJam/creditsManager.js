d84ce7985ad73c8a8817b21b553c7329936a1bfd
huynh01 <bhuyn17@gmail.com>
2022-11-12 20:31:08 +1000

    added credits for punch and damage sounds

--- gameJam/creditsManager.js
+++ gameJam/creditsManager.js
@@ -15,11 +15,24 @@ class creditsManager{
         textAlign(CENTER, CENTER)
         text('Credits', width/2, 100);
         pop();
-        for (let i = 0; i < creditList.credits.length; i++){
+        push();
+        stroke('white');
+        strokeWeight(10);
+        line(width/2, 150, width/2, height - 50);
+        pop();
+        for (let i = 0; i < creditList.creators.length; i++){
+            textSize(25);
+            text(creditList.creators[i].Title, width/2 - 300, i * 75 + 210);
             textSize(20);
-            text(creditList.credits[i].Title, width/2 - 400, i * 75 + 210);
-            text(creditList.credits[i].Name, width/2 - 380, i * 75 + 230);
-            text(creditList.credits[i].Link, width/2 - 360, i * 75 + 255);
+            text(creditList.creators[i].Name, width/2 - 280, i * 75 + 230);
+            text(creditList.creators[i].Link, width/2 - 260, i * 75 + 255);
+        }
+        for (let i = 0; i < creditList.credits.length; i++){
+            textSize(25);
+            text(creditList.credits[i].Title, width/2 + 100, i * 75 + 210);
+            textSize(20);
+            text(creditList.credits[i].Name, width/2 + 120, i * 75 + 230);
+            text(creditList.credits[i].Link, width/2 + 140, i * 75 + 255);
         }
     }
 }
