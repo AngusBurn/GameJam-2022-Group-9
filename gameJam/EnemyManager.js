a97efc514f8d49e205727e1fa287a5fb981d7bc1
huynh01 <bhuyn17@gmail.com>
2022-11-13 11:27:29 +1000

    removed debug and other console logs

--- gameJam/EnemyManager.js
+++ gameJam/EnemyManager.js
@@ -65,7 +65,7 @@ class EnemyManager{
       tempEnemy.stun = false;
       tempEnemy.attractionPoint(1,tempPlayer.position.x,tempPlayer.position.y)
       tempEnemy.setCollider('rectangle',0,0,23,43)
-      tempEnemy.debug = true;
+      // tempEnemy.debug = true;
       tempEnemy.addAnimation("idle",enemySpriteIdle)
       tempEnemy.addAnimation("walk",enemyWalk1,enemyWalk2,enemyWalk3);
       tempEnemy.addAnimation("punch",enemyPunch)
@@ -148,7 +148,7 @@ class EnemyManager{
             }
   
             if(attackMode == true && movement == false){
-              console.log("I punch thee")
+              // console.log("I punch thee")
               if(this.enemyGroup[i].mirrorX() == 1){
                 this.enemyGroup[i].setCollider('rectangle', 20, 0, 83, 43)
               } else if(this.enemyGroup[i].mirrorX() == -1){
@@ -210,7 +210,7 @@ class EnemyManager{
       if(tempPlayer.currentHP <= 5){
         this.enemyGroup.overlap(this.playerGroup,this.removePlayer)
         currentScreen = GAMEOVER;
-        console.log("you fuggen dead :DDDDD");
+        // console.log("you fuggen dead :DDDDD");
       }
       
 
